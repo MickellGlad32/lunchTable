e4a1f6082ad039c4b25199309f9fbee040582482
Mickell Gladness <mickellgladness@gmail.com>
2021-08-03 17:08:11 -0400

    finished favorites

--- public/javascripts/edamamApi.js
+++ public/javascripts/edamamApi.js
@@ -26,21 +26,25 @@ searchForm.addEventListener('submit', (event) => {
         })
 
     function renderRecipes(recipesArray) {
-        let recipesHtmlArray = recipesArray.map((result) => {
-            console.log(result)
+        console.log(recipesArray[0])
+        let recipesHtmlArray = recipesArray.map((result, index) => {
         
             const {image, label, source, url,ingredients,id, dietLabels} = result.recipe
+            const ingredientsList = ingredients.map((ingredient, index) =>{
+                return `<input type="hidden" value="${ingredient.text}" name="ingredients">`
+            })
+            console.log(ingredientsList)
             return `
             <div class="card-img-top" style="width: 18rem;">
                     <img src="${image}" class=" card-img-top border border-2 border-light shadow p-3  bg-body rounded" alt="...">
                     <div class="card-body mb-3">
                         <h5 id="recipe-name"class="card-title">${label}</h5>
                         <p id="recipe-source" class="card-text">${source}</p>
-                        <form method="post" action="/api/v1/recipes">
+                        <form method="post" action="/api/v1/favorites">
                         <input type="hidden" value="${label}" name="title">
                         <input type="hidden" value="${dietLabels[0]} name="category">
                         <input type="hidden" value="${url}" name="instructions">
-                        <input type="hidden" value="${ingredients[0].text}" name="ingredients"> 
+                        ${ingredientsList}
                         <button type="submit">Add favorite recipe</button>
                         </form>
                         <a id="eventButton" data-id="${id}" data-name="${label}" href="./results.html" class="btn btn-outline-light eventButton">Recipe Page</a>
--- routes/api/Favorites.js
+++ routes/api/Favorites.js
@@ -3,64 +3,93 @@ const db = require('../../models')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-db.Favorite.findAll({
+  db.Favorite.findAll({
     include: [db.User, db.Recipe]
-})
-.then((favorites) => {
-    res.json(favorites)
+  })
+    .then((favorites) => {
+      res.json(favorites)
 
-})
+    })
 })
 
 // ??????????
 // POST /api/v1/favorite/:id
 router.post('/:id', (req, res) => {
-    // check for all required fields
-  
-    // add fav to recipe table if from API??
+  // check for all required fields
 
-    // check if user exists
-    db.User.findByPk(req.session.user.id)
-      .then((user) => {
-        // if there is no user, respond with 404
-        if (!user) {
-          res.status(404).json({ error: 'could not find the user ' })
-          return
-        }
-        db.Recipe.findByPk(req.params.id).then(recipe => {          
-          // create new favotite        
-          db.Favorite.create({
-            UserId: user.id,
-            RecipeId: recipe.id,
-            title: recipe.title
-          }).then((favorite) => {
-              // respond with new favorite
-              res.status(201).json(favorite)
-            })
+  // add fav to recipe table if from API??
+
+  // check if user exists
+  db.User.findByPk(req.session.user.id)
+    .then((user) => {
+      // if there is no user, respond with 404
+      if (!user) {
+        res.status(404).json({ error: 'could not find the user ' })
+        return
+      }
+      db.Recipe.findByPk(req.params.id).then(recipe => {
+        // create new favotite        
+        db.Favorite.create({
+          UserId: user.id,
+          RecipeId: recipe.id,
+          title: recipe.title
+        }).then((favorite) => {
+          // respond with new favorite
+          res.status(201).json(favorite)
         })
       })
-  })
-  router.post('/', function(req, res, next) {
-    // check for all required fields
-    if (!req.body || !req.body.title ||!req.body.ingredients || !req.body.instructions) {
-      // if not all, send error
-      res.status(422).json({ error: 'must include title, ingredients and instructions' })
-      return
-    }
-    // create a new recipes
-    db.Recipe.create({
+    })
+})
+router.post('/', function (req, res, next) {
+  // check for all required fields
+  if (!req.body || !req.body.title || !req.body.ingredients || !req.body.instructions) {
+    // if not all, send error
+    res.status(422).json({ error: 'must include title, ingredients and instructions' })
+    return
+  }
+
+  // create a new recipes
+  db.Recipe.findOrCreate({
+    where: {
+      instructions: req.body.instructions,
+
+    },
+    defaults: {
       title: req.body.title,
       category: req.body.category,
-      ingredients: req.body.ingredients,
-      instructions: req.body.instructions       
-    })
-      .then((recipe) => {
-        db.Instruction.create({
-          steps: recipe.instructions,
-          RecipeId: recipe.id,
-        })
+      ingredients: req.body.ingredients.join(", "),
+      instructions: req.body.instructions
+
+    }
+  })
+    .then((recipe) => {
+      console.log(recipe[0].id)
+      db.Favorite.findOrCreate({
+        where: {
+          UserId: req.session.user.id,
+          RecipeId: recipe[0].id
+        }
+      }).then(() => {
         // send new recipe as response
         res.status(201).json(recipe)
       })
-  });
+    })
+});
+router.delete('/api/v1/favorites/:id', (req, res) => {
+  // get id
+  const id = parseInt(req.params.id)
+
+  db.result("DELETE FROM FAVORITES WHERE id = $1", id)
+    .then((results) => {
+      // if nothing was deleted, then there was nothing to delete
+      if (results.rowCount === 0) {
+        res.status(404).json({ error: 'could not find favorite with that id' })
+
+      } else {
+        res.status(204).json()
+
+      }
+    })
+
+})
 module.exports = router;
